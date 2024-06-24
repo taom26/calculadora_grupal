51a27f6f929f8930db744321702c4e584a4ea1eb
Gabriel Davila <114791590+GaboXxX26@users.noreply.github.com>
2024-06-23 21:59:25 -0500

    calculadora v1

---
+++ lib/CustomWidgets/CustomButton.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+// Define el widget personalizado CustomButton
+class CustomButton extends StatelessWidget {
+  final String label;
+  final Color color;
+  final VoidCallback onPressed;
+
+  CustomButton({
+    required this.label,
+    required this.color,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(5.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          padding: const EdgeInsets.all(20.0),
+          textStyle: const TextStyle(fontSize: 24.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        child: Text(label),
+      ),
+    );
+  }
+}
--- lib/Screens/CalculadoraAvanzada.dart
+++ lib/Screens/CalculadoraAvanzada.dart
@@ -7,8 +7,7 @@ import 'package:provider/provider.dart';
 import 'package:calculadora_grupal/CustomWidgets/CustomDrawer.dart'; // Importa el nuevo widget personalizado
 
 class CalculadoraAvanzada extends StatefulWidget {
-  const CalculadoraAvanzada({Key? key})
-      : super(key: key); // Corrige el constructor
+  const CalculadoraAvanzada({Key? key}) : super(key: key);
 
   @override
   _CalculadoraAvanzadaState createState() => _CalculadoraAvanzadaState();
@@ -17,9 +16,62 @@ class CalculadoraAvanzada extends StatefulWidget {
 class _CalculadoraAvanzadaState extends State<CalculadoraAvanzada> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
-  @override
-  void initState() {
-    super.initState();
+  String _output = "0";
+  String _input = "";
+  double _num1 = 0;
+  double _num2 = 0;
+  String _operator = "";
+
+  void _buttonPressed(String buttonText) {
+    if (buttonText == "C") {
+      _input = "";
+      _num1 = 0;
+      _num2 = 0;
+      _operator = "";
+      _output = "0";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "x" ||
+        buttonText == "÷") {
+      _num1 = double.parse(_input);
+      _operator = buttonText;
+      _input = "";
+    } else if (buttonText == "=") {
+      _num2 = double.parse(_input);
+      if (_operator == "+") {
+        _output = (_num1 + _num2).toString();
+      } else if (_operator == "-") {
+        _output = (_num1 - _num2).toString();
+      } else if (_operator == "x") {
+        _output = (_num1 * _num2).toString();
+      } else if (_operator == "÷") {
+        _output = (_num1 / _num2).toString();
+      }
+      _input = _output;
+      _operator = "";
+    } else {
+      _input += buttonText;
+      _output = _input;
+    }
+
+    setState(() {});
+  }
+
+  Color _getButtonColor(String label) {
+    switch (label) {
+      case "C":
+      case "⌫":
+      case "%":
+        return Colors.redAccent.shade100;
+      case "÷":
+      case "x":
+      case "-":
+      case "+":
+      case "=":
+        return Colors.yellowAccent.shade700;
+      default:
+        return Colors.greenAccent.shade100;
+    }
   }
 
   @override
@@ -52,7 +104,233 @@ class _CalculadoraAvanzadaState extends State<CalculadoraAvanzada> {
         },
       ),
       drawer: const CustomDrawer(), // Usa el widget personalizado CustomDrawer
-      body: const Center(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.centerRight,
+            padding:
+                const EdgeInsets.symmetric(vertical: 15.0, horizontal: 30.0),
+            child: Text(
+              _output,
+              style: const TextStyle(
+                fontSize: 50.0,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          const Expanded(
+            child: Divider(),
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: CustomButton(
+                        label: "%",
+                        color: _getButtonColor("%"),
+                        onPressed: () => _buttonPressed("%")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "C",
+                        color: _getButtonColor("C"),
+                        onPressed: () => _buttonPressed("C")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "⌫",
+                        color: _getButtonColor("⌫"),
+                        onPressed: () => _buttonPressed("⌫")),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: CustomButton(
+                        label: "1/x",
+                        color: _getButtonColor("1/x"),
+                        onPressed: () => _buttonPressed("1/x")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "x^2",
+                        color: _getButtonColor("x^2"),
+                        onPressed: () => _buttonPressed("x^2")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "√",
+                        color: _getButtonColor("√"),
+                        onPressed: () => _buttonPressed("√")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "÷",
+                        color: _getButtonColor("÷"),
+                        onPressed: () => _buttonPressed("÷")),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: CustomButton(
+                        label: "7",
+                        color: _getButtonColor("7"),
+                        onPressed: () => _buttonPressed("7")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "8",
+                        color: _getButtonColor("8"),
+                        onPressed: () => _buttonPressed("8")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "9",
+                        color: _getButtonColor("9"),
+                        onPressed: () => _buttonPressed("9")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "x",
+                        color: _getButtonColor("x"),
+                        onPressed: () => _buttonPressed("x")),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: CustomButton(
+                        label: "4",
+                        color: _getButtonColor("4"),
+                        onPressed: () => _buttonPressed("4")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "5",
+                        color: _getButtonColor("5"),
+                        onPressed: () => _buttonPressed("5")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "6",
+                        color: _getButtonColor("6"),
+                        onPressed: () => _buttonPressed("6")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "-",
+                        color: _getButtonColor("-"),
+                        onPressed: () => _buttonPressed("-")),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: CustomButton(
+                        label: "1",
+                        color: _getButtonColor("1"),
+                        onPressed: () => _buttonPressed("1")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "2",
+                        color: _getButtonColor("2"),
+                        onPressed: () => _buttonPressed("2")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "3",
+                        color: _getButtonColor("3"),
+                        onPressed: () => _buttonPressed("3")),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                        label: "+",
+                        color: _getButtonColor("+"),
+                        onPressed: () => _buttonPressed("+")),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: CustomButton(
+                      label: "+/-",
+                      color: _getButtonColor("+/-"),
+                      onPressed: () => _buttonPressed("+/-"),
+                    ),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                      label: "0",
+                      color: _getButtonColor("0"),
+                      onPressed: () => _buttonPressed("0"),
+                    ),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                      label: ".",
+                      color: _getButtonColor("."),
+                      onPressed: () => _buttonPressed("."),
+                    ),
+                  ),
+                  Expanded(
+                    child: CustomButton(
+                      label: "=",
+                      color: _getButtonColor("="),
+                      onPressed: () => _buttonPressed("="),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Define el widget personalizado CustomButton
+class CustomButton extends StatelessWidget {
+  final String label;
+  final Color color;
+  final VoidCallback onPressed;
+
+  CustomButton({
+    required this.label,
+    required this.color,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(6.0),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          padding: const EdgeInsets.all(18.0),
+          textStyle: const TextStyle(fontSize: 24.0),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(125.0),
+          ),
+        ),
+        child: Text(label),
+      ),
     );
   }
 }
